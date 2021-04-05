6ac582f3a1f568569e320f64b6fccc018dcd96c1
Li <lianplass@gmail.com>
2021-04-05 05:56:11 -0400

    Provides R script for Shiny Web App

---
+++ analysis_functions.R
@@ -0,0 +1,130 @@
+#The following is a general Shiny App used to display statistics, charts, and graphs based on sanitized user input data.
+
+# REMEMBER TO CHANGE THESE DEFAULT VALUES TO FUNCTION THAT READS CORRESPONDING INPUT CSV COLUMN
+
+# COPY FROM EXAMPLE SCRIPT
+basic_dataset <- read.csv("C:/Users/Lian Plass/Downloads/_TEMP/JAX-CHNA/CHNA_Analysis/basic_dataset.csv", stringsAsFactors=FALSE)
+
+var_obs_1<-input_csv[,1]
+var_obs_2<-input_csv[,2]
+var_obs_3<-input_csv[,3]
+var_obs_4<-input_csv[,4]
+var_obs_5<-input_csv[,5]
+var_obs_6<-input_csv[,6]
+var_obs_7<-input_csv[,7]
+var_obs_8<-input_csv[,8]
+var_obs_9<-input_csv[,9]
+var_obs_10<-input_csv[,10]
+
+all_var_obs<-c("var_obs_1","var_obs_2","var_obs_3","var_obs_4","var_obs_5","var_obs_6","var_obs_7","var_obs_8","var_obs_9","var_obs_10")
+
+descriptives_table<-(0)
+
+input_mean<-c(0)
+input_median<-c(0)
+input_mode<-c(0)
+input_range<-c(0)
+input_SD<-c(0)
+input_variance<-c(0)
+input_count<-c(0)
+
+possible_functions<-c("mean_calc","median_calc", "mode_calc","range_calc","SD_calc","vari_calc", "count_calc")
+
+#MEAN
+mean_calc <- function(var_observations){
+  input_mean<<-mean(var_observations)
+}
+
+#MEDIAN
+median_calc <- function(var_observations){
+  input_median<<-median(var_observations)
+}
+
+#MODE
+mode_calc <- function(var_observations){
+  uniquevar <-unique(var_observations)
+  input_mode<<-uniquevar[which.max(tabulate(match(var_observations, uniquevar)))]
+}
+
+#RANGE
+range_calc <- function(var_observations){
+  input_range <<-max(var_observations)-min(var_observations)
+}
+
+#STANDARD DEVIATION
+SD_calc <- function(var_observations){
+  input_SD<<-sd(var_observations)
+}
+
+#VARIANCE
+vari_calc <- function(var_observations){
+  input_variance<<-var(var_observations)
+}
+
+#COUNT (NOT WORD FREQUENCY ANALYSIS)
+count_calc <- function(var_observations){
+  input_count<<-table(var_observations)
+}
+
+#RETURN TO THIS WEEK OF 4-4-2021--FIX FUNCTION TO PROCESS EACH VARIABLE
+descriptives_table_function<-function(){
+  for (i in all_var_obs) {
+    for (j in possible_functions){
+      descriptives_table<<-parse(text=j)(all_var_obs[i])
+    }
+  }
+}
+
+descriptives_table_function()
+# SHINY APPLICATION BEGINS HERE
+
+
+library(shiny)
+
+ui<- fluidPage(
+  titlePanel("Quick Survey Analytics"),
+  tags$p("So you just conducted a community survey--AWESOME"),
+  tags$p("Now...what to do with all that sweet, sweet data?  Use (and adapt) this tool to get some quick analytics...without the code."),
+  sidebarLayout(
+    #This is the left sidebar containing operation checkboxes
+    sidebarPanel(
+      fileInput(
+        inputId = "input_csv", 
+        label = "Upload your sanitized CSV here:",
+        accept = c("text/csv", "text/comma-separated-values,text/plain",
+                   ".csv")
+        ),
+      tags$hr(),
+      checkboxInput(
+        inputId = "header", 
+        label = "Header",
+        value = TRUE
+        ),
+      tableOutput("descriptive_statistics"),
+      tableOutput("test")
+    ),
+  #This is the main panel containing the descriptive statistic plots and    the word cloud analysis
+  mainPanel(
+    tags$p("PLACEHOLDER"),
+    plotOutput("descriptive_statistics_plots"),
+    plotOutput("word_cloud_plots")
+  )
+)
+)
+
+server<- function(input, output){
+  output$descriptive_statistics<-renderTable({
+    inFile <-input$input_csv
+    if(is.null(inFile))
+      return(NULL)
+    read.csv(inFile$datapath, header = input$header)
+      #observe({
+    #output$value<- ADD SELECTION IN A SPECIFIC ORDER IN A LIST
+  #output$descriptive_statistics_plots
+  #output$word_cloud_plots
+  
+  }
+  output$test<-renderTable(descriptives_table))
+}
+
+shinyApp(ui=ui, server=server)
